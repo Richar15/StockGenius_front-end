719eaee6d8b9295dc93d98838a4f0e10d9424102
Richard15 <richardantonioassistrujillo@gmail.com>
2024-11-19 11:16:14 -0500

    refactor: improve error handling and formatting in quotation component

--- src/components/quotation/quotation.component.ts
+++ src/components/quotation/quotation.component.ts
@@ -2,41 +2,47 @@ import { Component } from '@angular/core';
 import { QuotationService } from '../../service/quotation.service';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
-
-
 @Component({
   selector: 'app-quotation',
   templateUrl: './quotation.component.html',
-  styleUrl: './quotation.component.css'
+  styleUrl: './quotation.component.css',
 })
 export class QuotationComponent {
   clientName: string = '';
   selectedClient: any;
   clientResults: any[] = [];
-  selectedProducts: { name: string, amount: number }[] = [];
+  selectedProducts: { name: string; amount: number }[] = [];
   productName: string = '';
   productAmount: number = 0;
   productResults: any[] = [];
   selectedProduct: any;
 
-  constructor(private quotationService: QuotationService, private router: Router) {}
-
+  constructor(
+    private quotationService: QuotationService,
+    private router: Router
+  ) {}
 
   searchClient(name: string) {
-
     if (name.trim() === '') {
       this.clientResults = [];
       return;
     }
-    this.quotationService.searchClientsByName(name).subscribe(clients => {
-      if (clients.length > 0) {
-        this.clientResults = clients;
-      } else {
-        Swal.fire('Error', 'Ocurrió un error al buscar clientes.', 'error');
+    this.quotationService.searchClientsByName(name).subscribe(
+      (clients) => {
+        if (clients.length > 0) {
+          this.clientResults = clients;
+        } else {
+          Swal.fire('Error', 'Ocurrió un error al buscar clientes.', 'error');
+        }
+      },
+      (error) => {
+        Swal.fire(
+          'Cliente no encontrado',
+          'No se encontraron clientes que coincidan con su búsqueda.',
+          'warning'
+        );
       }
-    }, error => {
-      Swal.fire('Cliente no encontrado', 'No se encontraron clientes que coincidan con su búsqueda.', 'warning');
-    });
+    );
   }
 
   selectClient(client: any) {
@@ -46,21 +52,27 @@ export class QuotationComponent {
   }
 
   searchProduct(name: string) {
-
     if (name.trim() === '') {
       this.productResults = [];
       return;
     }
 
-    this.quotationService.searchProductsByName(name).subscribe(products => {
-      if (products.length > 0) {
-        this.productResults = products;
-      } else {
-        Swal.fire('Error', 'Ocurrió un error al buscar productos.', 'error');
+    this.quotationService.searchProductsByName(name).subscribe(
+      (products) => {
+        if (products.length > 0) {
+          this.productResults = products;
+        } else {
+          Swal.fire('Error', 'Ocurrió un error al buscar productos.', 'error');
+        }
+      },
+      (error) => {
+        Swal.fire(
+          'Producto no encontrado',
+          'No se encontraron productos que coincidan con su búsqueda.',
+          'warning'
+        );
       }
-    }, error => {
-      Swal.fire('Producto no encontrado', 'No se encontraron productos que coincidan con su búsqueda.', 'warning');
-    });
+    );
   }
 
   selectProduct(product: any) {
@@ -81,16 +93,19 @@ export class QuotationComponent {
   createQuotation() {
     const quotationData = {
       clientName: this.clientName,
-      selectedProducts: this.selectedProducts
+      selectedProducts: this.selectedProducts,
     };
 
-    this.quotationService.reateQuotation(quotationData).subscribe((response: Blob) => {
-      const blob = new Blob([response], { type: 'application/pdf' });
-      const url = window.URL.createObjectURL(blob);
-      window.open(url);
-    }, error => {
-      Swal.fire('Error', 'Ocurrió un error al crear la cotización.', 'error');
-    });
+    this.quotationService.reateQuotation(quotationData).subscribe(
+      (response: Blob) => {
+        const blob = new Blob([response], { type: 'application/pdf' });
+        const url = window.URL.createObjectURL(blob);
+        window.open(url);
+      },
+      (error) => {
+        Swal.fire('Error', 'Ocurrió un error al crear la cotización.', 'error');
+      }
+    );
   }
 
   back(): void {
